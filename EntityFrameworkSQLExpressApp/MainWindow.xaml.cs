3cd9cb2bbd26004aa3fe00af86dbfc03ac5cb70f
yqin01 <yqin01@beckman.com>
2016-08-07 21:17:33 -0700

    Add database

--- EntityFrameworkSQLExpressApp/MainWindow.xaml.cs
+++ EntityFrameworkSQLExpressApp/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace EntityFrameworkSQLExpressApp
 {
@@ -20,23 +9,18 @@ namespace EntityFrameworkSQLExpressApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        ClassDbEntities entities;
         public MainWindow()
         {
             InitializeComponent();
-
-            entities = new ClassDbEntities();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-
-            EntityFrameworkSQLExpressApp.ClassDbDataSet classDbDataSet = ((EntityFrameworkSQLExpressApp.ClassDbDataSet)(this.FindResource("classDbDataSet")));
-            // Load data into the table Student. You can modify this code as needed.
-            EntityFrameworkSQLExpressApp.ClassDbDataSetTableAdapters.StudentTableAdapter classDbDataSetStudentTableAdapter = new EntityFrameworkSQLExpressApp.ClassDbDataSetTableAdapters.StudentTableAdapter();
-            classDbDataSetStudentTableAdapter.Fill(classDbDataSet.Student);
-            System.Windows.Data.CollectionViewSource studentViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("studentViewSource")));
-            studentViewSource.View.MoveCurrentToFirst();
+            using (ClassDbEntities entities = new ClassDbEntities())
+            {
+                entities.Students.Load();
+                dataGrid.ItemsSource = entities.Students.Local;
+            }
         }
     }
 }
