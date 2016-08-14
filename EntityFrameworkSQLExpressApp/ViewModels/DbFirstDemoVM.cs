f35df75394e6094b73dcee513cbb0b1a23377a14
yqin01 <yqin01@beckman.com>
2016-08-14 12:22:42 -0700

    doc

--- EntityFrameworkSQLExpressApp/ViewModels/DbFirstDemoVM.cs
+++ EntityFrameworkSQLExpressApp/ViewModels/DbFirstDemoVM.cs
@@ -88,6 +88,7 @@ namespace EntityFrameworkSQLExpressApp.ViewModels
         #endregion
 
         #region commands
+        #region public
         ICommand _InsertCommand;
         public ICommand InsertCommand
         {
@@ -102,25 +103,78 @@ namespace EntityFrameworkSQLExpressApp.ViewModels
             }
         }
 
+        ICommand _DeleteCommand;
+        public ICommand DeleteCommand
+        {
+            get
+            {
+                if (_DeleteCommand == null)
+                {
+                    _DeleteCommand = new RelayCommand<string>((param) => { OnDeleteCommand(param); },
+                                                              (param) => { return true; });
+                }
+                return _DeleteCommand;
+            }
+        }
+
+        #endregion
+        #region private
         void OnInsertCommand(string param)
         {
             if(param == "Student")
             {
-                ;
+                _DbContext.Students.Add(new Student() { Name = "Jerry" });
+                _DbContext.Students.Add(new Student() { Name = "Lisa" });
+                _DbContext.SaveChanges();
             }
             else if (param == "Teacher")
             {
-                ;
+                _DbContext.Teachers.Add(new Teacher() { Name = "Kim" });
+                _DbContext.Teachers.Add(new Teacher() { Name = "Clause" });
+                _DbContext.SaveChanges();
             }
             else if (param == "Course")
             {
-                ;
+                _DbContext.Courses.Add(new Course() { Name = "Arts" });
+                _DbContext.Courses.Add(new Course() { Name = "Biology" });
+                _DbContext.SaveChanges();
             }
             else
             {
                 ;
             }
         }
+
+        void OnDeleteCommand(string param)
+        {
+            if (param == "Student")
+            {
+                Student student = _DbContext.Students.FirstOrDefault(s => s.Name == "Lisa");
+                if(student != null)
+                    _DbContext.Students.Remove(student);
+                student = _DbContext.Students.FirstOrDefault(s => s.Name == "Jerry");
+                if (student != null)
+                    _DbContext.Students.Remove(student);
+                _DbContext.SaveChanges();
+            }
+            else if (param == "Teacher")
+            {
+                Teacher teacher = _DbContext.Teachers.FirstOrDefault(t => t.Name == "Clause");
+                _DbContext.Teachers.Remove(teacher);
+                _DbContext.SaveChanges();
+            }
+            else if (param == "Course")
+            {
+                Course course = _DbContext.Courses.FirstOrDefault(c => c.Name == "Arts");
+                _DbContext.Courses.Remove(course);
+                _DbContext.SaveChanges();
+            }
+            else
+            {
+                ;
+            }
+        }
+        #endregion
         #endregion
     }
 }
